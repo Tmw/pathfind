9d97bcfa457789be17f2733be719b2d7de1c6cc4
Tiemen Waterreus <tiemen@tiemenwaterreus.com>
2024-01-07 17:26:09 +0100

    Include start node in path when doing backtrace

--- backtrace.go
+++ backtrace.go
@@ -6,8 +6,11 @@ func backtrace[T comparable](c candidate[T]) []T {
 	path := []T{}
 
 	hop := c
-	for hop.parent != nil {
+	for {
 		path = append(path, hop.coord)
+		if hop.parent == nil {
+			break
+		}
 		hop = *hop.parent
 	}
 
